44dc88fd3e2d1f8a0ecca73052b562912faf8db4
胡世金 <18047586@cnsuning.com>
2019-04-04 14:26:54 +0800

    编写一个函数来查找字符串数组中的最长公共前缀

---
+++ src/com/question/bank/leedcode/easy/LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+package com.question.bank.leedcode.easy;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+    如果不存在公共前缀，返回空字符串 ""。
+ */
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static  String longestCommonPrefix(String[] strs) {
+        //1.取前两个字符串的公共部分
+        //2.将最后一个字符串与公共部分比较是否存在公共部分
+
+        return "";
+    }
+
+}
--- src/com/question/bank/test/TestMain.java
+++ src/com/question/bank/test/TestMain.java
@@ -1,11 +1,52 @@
 package com.question.bank.test;
 
-import com.question.bank.leedcode.easy.AddTwoNumbers;
-
+/**
+ * java浮点数运算中有两个特殊的情况：NAN、INFINITY
+ */
 public class TestMain {
+    // jdk 源码 Double Float 定义
+    /**
+     * A constant holding the positive infinity of type
+     * {@code double}. It is equal to the value returned by
+     * {@code Double.longBitsToDouble(0x7ff0000000000000L)}.
+     */
+    public static final double POSITIVE_INFINITY = 1.0 / 0.0;
 
-	public static void main(String[] args) {
-		AddTwoNumbers t = new AddTwoNumbers();
-	}
+    /**
+     * A constant holding the negative infinity of type
+     * {@code double}. It is equal to the value returned by
+     * {@code Double.longBitsToDouble(0xfff0000000000000L)}.
+     */
+    public static final double NEGATIVE_INFINITY = -1.0 / 0.0;
+
+    /**
+     * A constant holding a Not-a-Number (NaN) value of type
+     * {@code double}. It is equivalent to the value returned by
+     * {@code Double.longBitsToDouble(0x7ff8000000000000L)}.
+     */
+    public static final double NaN = 0.0d / 0.0;
+
+    // 7 源码
+
+    public static void main(String[] args) {
+        System.out.println(POSITIVE_INFINITY);
+        System.out.println(NEGATIVE_INFINITY);
+        System.out.println(NaN);
+        System.out.println("---------------------");
+        System.out.println(POSITIVE_INFINITY * 0);
+        System.out.println(POSITIVE_INFINITY * 1);
+        System.out.println(POSITIVE_INFINITY / 0);
+        System.out.println(POSITIVE_INFINITY + 1000);
+        System.out.println("---------------------");
+        System.out.println(NEGATIVE_INFINITY * 0);
+        System.out.println(NEGATIVE_INFINITY * 1);
+        System.out.println(NEGATIVE_INFINITY / 0);
+        System.out.println(NEGATIVE_INFINITY + 1000);
+        System.out.println("---------------------");
+        System.out.println(NaN * 0);
+        System.out.println(NaN * 1);
+        System.out.println(NaN / 0);
+        System.out.println(NaN + 1000);
+    }
 
 }
